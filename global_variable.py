08218816890f536e92272227fdb92120a13e580d
avnishahlawat <avnishahlawat13@gmail.com>
2025-08-27 14:16:39 +0530

    sample program using global variable

---
+++ global_variable.py
@@ -0,0 +1,13 @@
+# Global variable
+count = 0  
+
+def increment():
+    global count   # declaring count as global
+    count += 1
+    print("Inside function, count =", count)
+
+# Main program
+print("Initial count =", count)
+increment()
+increment()
+print("Final count =", count)
