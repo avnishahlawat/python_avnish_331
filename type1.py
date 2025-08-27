96ad2d4807aabfb2d54a3d44fbd22bbdb28b27ad
avnishahlawat <avnishahlawat13@gmail.com>
2025-08-27 09:23:10 +0530

    third commit

--- type1.py
+++
@@ -1,9 +0,0 @@
-# integer to float 
-num_int = 12
-num_float = float(num_int)
-print("Integer to float:", num_float)
-
-#Float to integer 
-num_float = 13.456
-num_int = int(num_float)
-print("Float to int:" , num_int)
