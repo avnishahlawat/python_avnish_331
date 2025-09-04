9f52aff039c16cca4317d2a58deb392e6bcdabc4
avnishahlawat <avnishahlawat13@gmail.com>
2025-09-04 23:32:45 +0530

    Updated commit

---
+++ sample_input.py
@@ -0,0 +1,10 @@
+# Taking string input by the user 
+name = input("Enter your name: ")
+
+# Taking integer input by the user 
+v = int(input("Enter a number: "))
+
+print("Hello,", name)
+print("You entered the number:", v)
+
+
